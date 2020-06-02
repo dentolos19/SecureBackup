d56c29173e14cc44d522ecf2d7e0e5fd70f72f5f
Dennise <63337204+dentolos19@users.noreply.github.com>
2020-06-02 19:33:18 +0800

    Initial commit

---
+++ SecureBackup/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SecureBackup
+{
+
+    public partial class App
+    {
+
+        private void Initialize(object sender, StartupEventArgs args)
+        {
+            // todo
+        }
+
+    }
+}
